3aeaeb87fe3ac9ee08cc580a68dad6f2da06d805
Zach Moazeni <zach@collectiveidea.com>
2012-04-21 18:20:22 -0400

    Adding some leveldb tests

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Data.Char
 import Control.Monad
 import System.Exit
 import System.Random
+import Database.LevelDB
+import Data.ByteString.Char8 hiding (take, putStrLn, getLine, map)
+import Control.Monad.IO.Class (liftIO)
 
 data Action = Rock | Paper | Scissors | Quit | Invalid
             deriving Show
@@ -9,7 +14,11 @@ data Action = Rock | Paper | Scissors | Quit | Invalid
 data Result = Win | Lose | Tie
 
 main :: IO ()
-main = forever $ getLine >>= process
+main = do
+  runResourceT $ readValue
+  runResourceT $ saveValue "bar"
+  forever $ getLine >>= process
+          
   where randomAction = (randomRIO (1, 3) :: IO Int) >>= return . parseAction . show
         process s = case parseAction s of
           Quit -> putStrLn "exiting" >> exitWith ExitSuccess
@@ -19,6 +28,16 @@ main = forever $ getLine >>= process
                     Win  -> putStrLn $ "Your " ++ show c ++ " won against " ++ show r ++ "!"
                     Lose -> putStrLn $ "Your " ++ show c ++ " lost against " ++ show r ++ "."
                     Tie  -> putStrLn $ show c ++ " tied."
+                    
+saveValue :: MonadResource m => ByteString -> m ()
+saveValue value = do
+  db <- open "/tmp/zachtestldb" [CreateIfMissing, CacheSize 2048]
+  put db [] "foo" value
+  
+readValue :: MonadResource m => m ()
+readValue = do
+  db <- open "/tmp/zachtestldb" [CreateIfMissing, CacheSize 2048]
+  get db [] "foo" >>= liftIO . print
             
 parseAction :: String -> Action  
 parseAction a = case a' of
@@ -45,6 +64,4 @@ evaluate (Scissors, Rock)  = Lose
 
 evaluate (Paper, Rock)     = Win
 evaluate (Paper, Scissors) = Lose
-evaluate _                   = Tie
-
-        
+evaluate _                 = Tie
