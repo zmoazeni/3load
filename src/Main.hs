f9e36efe1871c5424c879f9c1e22a1c06fd2285f
Zach Moazeni <zach@collectiveidea.com>
2012-04-21 00:31:19 -0400

    Basic input parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,25 @@
+import Data.Char
+import Control.Monad
+import System.Exit
+
+data Action = Rock | Paper | Scissors | Quit | Invalid
+
 main :: IO ()
-main = putStrLn "Hello World"
+main = forever $ getLine >>= evaluateTurn
+  where evaluateTurn s = case parseAction s of
+          Rock -> putStrLn "stubborn as a rock"
+          Paper -> putStrLn "uh paper?"
+          Scissors -> putStrLn "dont cut yourself"
+          
+          Quit -> putStrLn "exiting" >> exitWith ExitSuccess
+          Invalid -> putStrLn "Unknown action. <rock|paper|scissors|quit>"
+    
+parseAction :: String -> Action  
+parseAction a = case a' of
+  ('r':_) -> Rock
+  ('p':_) -> Paper
+  ('s':_) -> Scissors
+  "quit"  -> Quit
+  _       -> Invalid
+  
+  where a' = map toLower a
